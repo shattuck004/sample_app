acb287a59a09d6d62fef46185590ebd1369e8048
Shattuck004 <katie.markov@gmail.com>
2013-01-11 12:11:23 -0800

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 }) 
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://us.cdn2.123rf.com/168nwm/synchr/synchr1201/synchr120100028/12044956-orange-crazy-smile-bored-theme-vector-illustration.jpg"
+    size = options[:size]
+    gravatar_url = "http://us.cdn2.123rf.com/168nwm/synchr/synchr1201/synchr120100028/12044956-orange-crazy-smile-bored-theme-vector-illustration.jpg?s={size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
