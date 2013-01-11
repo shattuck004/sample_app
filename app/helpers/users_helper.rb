156736d0117e60eac23484cb438dc6b9aaf0bcd1
Shattuck004 <katie.markov@gmail.com>
2013-01-11 11:26:40 -0800

    Clean database

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
-    # Returns the Gravatar (http://gravatar.com/) for the given user.
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    gravatar_url = "http://us.cdn2.123rf.com/168nwm/synchr/synchr1201/synchr120100028/12044956-orange-crazy-smile-bored-theme-vector-illustration.jpg"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
