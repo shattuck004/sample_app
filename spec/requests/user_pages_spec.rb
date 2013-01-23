a3ae93f3726c3d427ebeea00cfceaf0e8661b7f4
Shattuck004 <katie.markov@gmail.com>
2013-01-22 16:23:16 -0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,7 @@ describe "User pages" do
 
 describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
@@ -24,9 +25,17 @@ describe "profile page" do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "error messages" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -41,25 +50,16 @@ describe "profile page" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
 
-  describe "after saving the user" do
-    before { click_button submit }
-    let(:user) { User.find_by_email('user@example.com') }
+      describe "after saving the user" do
+       before { click_button submit }
+         let(:user) { User.find_by_email('user@example.com') }
 
-    it { should have_selector('title', text: user.name) }
-    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-  end
-
-describe "with invalid information" do
-  
-   describe "after submission" do
-      before { click_button submit }
-
-      it { should have_selector('title', text: 'Sign up') }
-      it { should have_content('error') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
     end
   end
 end
-
 
